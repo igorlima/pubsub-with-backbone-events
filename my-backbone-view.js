a6d78b18366f3b43504f85985295155d4a1863a0
Igor Ribeiro <U06IXR@macys.com>
2015-07-20 19:01:15 -0300

    refactoring

--- my-backbone-view.js
+++ my-backbone-view.js
@@ -10,7 +10,7 @@ define(['jquery', 'backbone', 'myModel', 'io', 'forceView', 'colorpicker'], func
 
     initialize: function( options ) {
       var view = this;
-      view.AppbaseSyncChannel = $.extend( {}, Backbone.Events );
+      view.dbChannel = $.extend( {}, Backbone.Events );
       view.model = new MyModel();
       view.$el.find('#editNodeModal #textColorNode').colorpicker();
 
@@ -32,7 +32,7 @@ define(['jquery', 'backbone', 'myModel', 'io', 'forceView', 'colorpicker'], func
     },
 
     editNode: function() {
-      this.AppbaseSyncChannel.trigger('edit-node', {
+      this.dbChannel.trigger('edit-node', {
         id: this.model.get('id'),
         color: this.$el.find('#textColorNode').val(),
         label: this.$el.find('#textNode').val()
@@ -41,39 +41,39 @@ define(['jquery', 'backbone', 'myModel', 'io', 'forceView', 'colorpicker'], func
     },
 
     addNode: function() {
-      this.AppbaseSyncChannel.trigger('add-node');
+      this.dbChannel.trigger('add-node');
     },
 
     sync: function() {
       this.syncWithForceView();
-      this.syncWithAppBase();
+      this.syncWithDBaaS();
       console.log('ready!! backbone view loaded and sync');
     },
 
-    syncWithAppBase: function() {
-      var socket, AppbaseChannel;
+    syncWithDBaaS: function() {
+      var socket, dbChannel;
       socket = io('https://dbas-with-socket-io.herokuapp.com/');
-      AppbaseChannel = this.AppbaseSyncChannel;
+      dbChannel = this.dbChannel;
 
-      AppbaseChannel.on('remove-node', function(node) {
+      dbChannel.on('remove-node', function(node) {
         socket.emit('remove-node', node);
       });
 
-      AppbaseChannel.on('add-link', function(link) {
+      dbChannel.on('add-link', function(link) {
         if (link.target.id) {
           socket.emit( 'add-link', link );
         }
       });
 
-      AppbaseChannel.on('remove-link', function(link) {
+      dbChannel.on('remove-link', function(link) {
         socket.emit('remove-link', link);
       });
 
-      AppbaseChannel.on('add-node', function(node, callback) {
+      dbChannel.on('add-node', function(node, callback) {
         socket.emit( 'add-node', node || {}, callback );
       } );
 
-      AppbaseChannel.on('edit-node', function(node) {
+      dbChannel.on('edit-node', function(node) {
         socket.emit( 'edit-node', node );
       });
 
@@ -103,12 +103,10 @@ define(['jquery', 'backbone', 'myModel', 'io', 'forceView', 'colorpicker'], func
     },
 
     syncWithForceView: function() {
-      var view, AppbaseChannel;
-      view = this;
-      AppbaseChannel = view.AppbaseSyncChannel;
+      var view = this;
 
       ForceView.channel.on('node-removed', function(node) {
-        AppbaseChannel.trigger( 'remove-node', node );
+        view.dbChannel.trigger( 'remove-node', node );
       });
 
       ForceView.channel.on('node-edited', function(node) {
@@ -121,15 +119,15 @@ define(['jquery', 'backbone', 'myModel', 'io', 'forceView', 'colorpicker'], func
       });
 
       ForceView.channel.on('link-added', function(link) {
-        AppbaseChannel.trigger( 'add-link', link );
+        view.dbChannel.trigger( 'add-link', link );
       });
 
       ForceView.channel.on('link-removed', function(link) {
-        AppbaseChannel.trigger('remove-link', link);
+        view.dbChannel.trigger('remove-link', link);
       });
 
       ForceView.channel.on('node-and-link-added', function(data) {
-        AppbaseChannel.trigger( 'add-node', data.node, function( node ) {
+        view.dbChannel.trigger( 'add-node', data.node, function( node ) {
           data.node.id = node.id;
           ForceView.channel.trigger('link-added', data.link);
         } );
